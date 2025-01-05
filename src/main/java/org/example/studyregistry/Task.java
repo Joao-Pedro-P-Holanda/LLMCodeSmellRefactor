1058d2d2aa802a22ae8ac8a5ee48826d13c69cfa
Francisco Paulino Arruda Filho <fpaulinofilho04@gmail.com>
2025-01-05 09:44:57 -0300

    fix: refactor DataClass code smell using the add method refactoring technique in studyregistry.Task.java

--- src/main/java/org/example/studyregistry/Task.java
+++ src/main/java/org/example/studyregistry/Task.java
@@ -1,42 +1,79 @@
 package org.example.studyregistry;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Task extends Registry{
-    private String title;
-    private String description;
-    private String author;
-    private LocalDateTime date;
+  private String title;
+  private String description;
+  private String author;
+  private LocalDateTime date;
 
-    public Task(String title, String description, String author, LocalDateTime date) {
-        this.title = title;
-        this.name = title;
-        this.description = description;
-        this.author = author;
-        this.date = date;
-    }
-    public String getTitle() {
-        return title;
-    }
-    public void setTitle(String title) {
-        this.title = title;
-    }
-    public String getDescription() {
-        return description;
-    }
-    public void setDescription(String description) {
-        this.description = description;
-    }
-    public String getAuthor() {
-        return author;
-    }
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-    public LocalDateTime getDate() {
-        return date;
-    }
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
+  public Task(String title, String description, String author, LocalDateTime date) {
+    this.title = title;
+    this.name = title;
+    this.description = description;
+    this.author = author;
+    this.date = date;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+    this.name = title; // Update name field as well when title is changed.
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public String getAuthor() {
+    return author;
+  }
+
+  public void setAuthor(String author) {
+    this.author = author;
+  }
+
+  public LocalDateTime getDate() {
+    return date;
+  }
+
+  public void setDate(LocalDateTime date) {
+    this.date = date;
+  }
+
+  public void setTitleAndDescription(String newTitle, String newDescription) {
+    this.title = newTitle;
+    this.description = newDescription;
+   }
+
+  // Added method to format date in a human-readable way
+  public String getFormattedDate() {
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+    return formatter.format(date);
+  }
+
+  public boolean isOverdue() {
+    LocalDateTime now = LocalDateTime.now();
+    return this.date.isBefore(now);
+  }
+
+  // Added toString method to provide a more informative representation of the Task object
+  @Override
+  public String toString() {
+    return "Task{" +
+        "title='" + title + '\'' +
+        ", description='" + description + '\'' +
+        ", author='" + author + '\'' +
+        ", date=" + date +
+        '}';
+  }
 }
