f1efcda9fd97812c7c0eceb69ab43ff4b19b54d9
Francisco Paulino Arruda Filho <fpaulinofilho04@gmail.com>
2025-01-05 10:55:57 -0300

    fix: refactor DataClass code smell using the add method refactoring technique in studymaterial.Reference.java

--- src/main/java/org/example/studymaterial/Reference.java
+++ src/main/java/org/example/studymaterial/Reference.java
@@ -103,5 +103,74 @@ public abstract class Reference {
         this.shareCount = shareCount;
     }
 
+     // Add methods for common operations
+     public Reference withTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public Reference withDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public Reference withLink(String link) {
+        this.link = link;
+        return this;
+    }
+
+    public Reference withAccessRights(String accessRights) {
+        this.accessRights = accessRights;
+        return this;
+    }
+
+    public Reference withLicense(String license) {
+        this.license = license;
+        return this;
+    }
+
+    public Reference withIsDownloadable(boolean isDownloadable) {
+        this.isDownloadable = isDownloadable;
+        return this;
+    }
+
+    public Reference withRating(int rating) {
+        this.rating = rating;
+        return this;
+    }
+
+    public Reference withLanguage(String language) {
+        this.language = language;
+        return this;
+    }
+
+    public Reference withViewCount(int viewCount) {
+        this.viewCount = viewCount;
+        return this;
+    }
+
+    public Reference withDownloadCount(int downloadCount) {
+        this.downloadCount = downloadCount;
+        return this;
+    }
+
+    public Reference withShareCount(int shareCount) {
+        this.shareCount = shareCount;
+        return this;
+    }
+
+    // Add methods for common actions
+    public void incrementViewCount() {
+        this.viewCount++;
+    }
+
+    public void incrementDownloadCount() {
+        this.downloadCount++;
+    }
+
+    public void incrementShareCount() {
+        this.shareCount++;
+    }
+
     public abstract void incrementCount(Map<String, Integer> referenceCount);
 }
